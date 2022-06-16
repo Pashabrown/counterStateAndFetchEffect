0b5b7df4f8b9f960328afad3b98193b9578a93c0
Pasha Brown <pashabrown@gmail.com>
2022-06-15 18:23:20 -0700

    counterstate and fetcheffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import * as React from "react";
 import './App.css';
+import axios from "axios";
+
+const {useEffect, useState} = React;
+
+
+// Make a request for a user with a given ID
+const fetchRandomData = ()=> {
+  return axios.get('https://randomuser.me/api')
+  .then(({data}) => {
+    // handle success
+    console.log(data);
+    return JSON.stringify(data);
+  })
+  .catch(err => {
+    // handle error
+    console.error(err);
+  });
+  
+}
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [randomUserDataJson, setRandomUserDataJson] = useState("");
+  
+  useEffect(() => {
+    fetchRandomData().then(randomData => {
+      setRandomUserDataJson(randomData || "No User Data");
+    });
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <p>
+        {counter}
+      </p>
+      <button onClick={() => {
+        setCounter(counter + 1);
+
+      }}>increase counter
+      </button>
+      <p>
+        {randomUserDataJson}
+      </p>
+      
+
+      
+
     </div>
   );
 }
